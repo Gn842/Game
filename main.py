0789edb4de66ea8c3b90fe080c71dde899b73cfc
Gn842 <natoguadalupe9@gmail.com>
2024-08-15 20:16:54 -0400

    Initialize basic Pygame setup with player and background rendering
    - Added Pygame initialization and screen setup.
    - Loaded and scaled player, bag, and background images.
    - Implemented basic player movement with arrow keys.
    - Set boundaries for player movement within the screen dimensions.
    - Added game loop with event handling and screen updates.

--- main.py
+++ main.py
@@ -9,16 +9,27 @@ screen = pygame.display.set_mode((screenWidth, screenHeight))
 
 player = pygame.image.load('./img/turtle.png')
 player = pygame.transform.scale(player, (100, 100))
+bag = pygame.image.load('./img/TrashBag.png')
+bag = pygame.transform.scale(bag, (120, 120))
 background = pygame.image.load('./img/water.jpg')
 background = pygame.transform.scale(background, (800, 800))
 
 run = True
-x= 100 
+x = 100 
 y = 100
+z = 120
+t = 120
+
+
+player_width = 100
+player_height = 100
+
 while run:
     screen.blit(background, (0, 0))
 
     screen.blit(player, (x, y))
+
+    screen.blit(bag, (z, t))
 
     key = pygame.key.get_pressed()
 
@@ -30,6 +41,16 @@ while run:
         y += 1
     elif key[pygame.K_UP] == True:
         y -= 1
+
+    if x < 0:
+        x = 0
+    if x > screenWidth - player_width:
+        x = screenWidth - player_width
+    if y < 0:
+        y = 0
+    if y > screenHeight - player_height:
+        y = screenHeight - player_height
+
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
